38a55870cbcae791a8c85d3d07675b9f42e933d4
tranchau <tranchau>
2023-02-22 23:24:01 +0700

    giao dien tao moi order: loading 70%

---
+++ src/app/entities/const-data.ts
@@ -0,0 +1,16 @@
+export const Cities = [
+  {value: 1, label: 'An Giang'},
+  {value: 2, label: 'Kien Giang'},
+  {value: 3, label: 'An Giang'},
+  {value: 4, label: 'An Giang'},
+  {value: 5, label: 'An Giang'},
+  {value: 6, label: 'An Giang'},
+  {value: 7, label: 'An Giang'},
+]
+
+export const Transports = [
+  {value: 1, label: 'Đường bộ'},
+  {value: 2, label: 'Đường thủy'},
+  {value: 3, label: 'Máy bay'},
+]
+
--- src/app/orders/order-create/order-create.component.ts
+++ src/app/orders/order-create/order-create.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Products } from '../../mock-data/products-data';
+import { Cities, Transports } from '../../entities/const-data';
+import { Order } from '../../entities/order';
 
 @Component({
   selector: 'app-order-create',
@@ -6,7 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./order-create.component.scss']
 })
 export class OrderCreateComponent {
+  cities: any[] = Cities;
+  products: any[] = Products;
+  transport: any[] = Transports;
+
+  receivedDate: Date = new Date();
   error: any = '';
+
+  order: Order = {
+    id: 0,
+    createdDate: '12/02/2023',
+    deliveryAddress: 'Can Tho',
+    pickupAddress: 'Kien Giang', 
+    productId: 2,
+    quantity: 20,
+    stranport: 1,
+    numberOfVehicles: 1,
+    driver: 'Thanh',
+    receivedDate: '20/02/2023',
+    note: '',
+    status: 0,
+  }
 
   onSubmit() {}
 
