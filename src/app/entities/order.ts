c14fea77795d9460f6b1ac4bafbaa84176e53056
tranchau <tranchau>
2023-03-03 08:32:57 +0700

    chinh sua hoan chinh UI order

--- src/app/entities/order.ts
+++ src/app/entities/order.ts
@@ -2,9 +2,12 @@ export interface Order {
     id: number;
     createdDate: string; // Ngày tạo đơn
     deliveryAddress: number; // Nơi nhận hàng
+    selectedDelivery?: string,
     pickupAddress: number, // Nơi giao hàng
+    selectedPickup?: string,
     productTotal: number,
     transport: number, // Phuong tien van chuyen (1: bộ, 2: thủy, 3: hàng không)
+    selectedTransport?: string,
     licensePlates: string, //biển số
     driver: string,
     receivedDate: string,
